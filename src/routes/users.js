154c038707b975892b1d39037f3aec8e1c7c8a93
Nikita <n.mikhailov55@gmail.com>
2022-10-03 01:57:50 +0300

    postman init , add hh.ru methods ;

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,31 +2,53 @@ import express from "express";
 const router = express.Router();
 import { v4 as uuid } from "uuid";
 
-const users = [
+let users = [
   {
+    id: "1",
     name: "John",
     lastName: "Fighter",
     age: "33",
   },
-  { name: "Max", lastName: "Azyk", age: "15" },
-  { name: "Nick", lastName: "Locz", age: "99" },
+  {
+    id: "2",
+    name: "Max",
+    lastName: "Azyk",
+    age: "15",
+  },
+  {
+    id: "3",
+    name: "Nick",
+    lastName: "Locz",
+    age: "99",
+  },
 ];
 
 router.get("/", (req, res) => {
-  console.log(users);
   res.send(users);
 });
 
 router.post("/", (req, res) => {
   const user = req.body;
-
   const uId = uuid();
-
   const userWithId = { id: uId, ...user };
 
   users.push(userWithId);
+  res.send(`user - ${userWithId.name} was added to mockdata`);
+});
 
-  res.send(`user - ${user} - WAS ADDED`);
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id == id);
+
+  res.send(`${foundUser.name}`);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  // const foundUser = users.find((user) => user.id == id);
+  users = users.filter((user) => user.id != id);
+
+  res.send(`${users}`);
 });
 
 export default router;
